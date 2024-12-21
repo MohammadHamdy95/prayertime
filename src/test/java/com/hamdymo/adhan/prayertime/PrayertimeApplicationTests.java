207b3243f6b90bbe3eac9778aa5fb59c6dd587e6
mohamdy <moham1255@gmail.com>
2024-12-21 13:05:41 -0800

    Initial Commit

---
+++ src/test/java/com/hamdymo/adhan/prayertime/PrayertimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamdymo.adhan.prayertime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrayertimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
