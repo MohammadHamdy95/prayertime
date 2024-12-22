bd847b5cd0acb1b009d02be2fe5e63d97626ed76
mohamdy <moham1255@gmail.com>
2024-12-21 22:38:35 -0800

    adding mp3 athaans

--- src/main/java/com/hamdymo/adhan/prayertime/PrayerTimeApplication.java
+++ src/main/java/com/hamdymo/adhan/prayertime/PrayerTimeApplication.java
@@ -2,12 +2,14 @@ package com.hamdymo.adhan.prayertime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class PrayertimeApplication {
+public class PrayerTimeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrayertimeApplication.class, args);
+		ApplicationContext apc = SpringApplication.run(PrayerTimeApplication.class);
+
 	}
 
 }
---
+++ src/main/java/com/hamdymo/adhan/prayertime/domain/model/DailyPrayerSchedule.java
@@ -0,0 +1,13 @@
+package com.hamdymo.adhan.prayertime.domain.model;
+public class DailyPrayerSchedule {
+
+    private String fajrTime;
+
+    private String dhurTime;
+
+    private String asrTime;
+
+    private String maghribTime;
+
+    private String ishaTime;
+}
