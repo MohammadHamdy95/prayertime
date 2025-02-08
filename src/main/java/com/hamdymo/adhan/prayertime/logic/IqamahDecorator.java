bb04405662955e9bb2f64cc703be23404c01dd15
mohamdy <moham1255@gmail.com>
2025-02-08 14:45:20 -0800

    fixed typo

--- src/main/java/com/hamdymo/adhan/prayertime/logic/IqamahDecorator.java
+++ src/main/java/com/hamdymo/adhan/prayertime/logic/IqamahDecorator.java
@@ -1,6 +1,8 @@
 package com.hamdymo.adhan.prayertime.logic;
 
 import com.hamdymo.adhan.prayertime.domain.model.DailyPrayerSchedule;
+import com.hamdymo.adhan.prayertime.domain.model.IqamahOffset;
+import com.hamdymo.adhan.prayertime.facade.FileFacade;
 import lombok.AllArgsConstructor;
 
 import java.util.HashMap;
@@ -9,14 +11,17 @@ import java.util.Map;
 @AllArgsConstructor
 public class IqamahDecorator {
     private DateFunctions dateFunctions;
+    private FileFacade fileFacade;
 
     public DailyPrayerSchedule addIqamahTimesToSchedule(DailyPrayerSchedule dailyPrayerSchedule) {
+        getIqamahConfig();
         return null;
     }
 
-
-    private Map<String, Integer> getIqamahMap() {
-        Map<String, Integer> iqamahMap = new HashMap<>();
-        return iqamahMap;
+    private IqamahOffset getIqamahConfig() {
+        IqamahOffset iqamahOffsetConfig = fileFacade.getIqamahOffsetConfiig();
+        System.out.println(iqamahOffsetConfig);
+        return null;
     }
+
 }
