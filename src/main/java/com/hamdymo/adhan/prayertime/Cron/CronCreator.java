92a23b4df39645517b076c8db5d527b36f0770d8
mohamdy <moham1255@gmail.com>
2025-02-08 03:12:37 -0800

    fixing url

--- src/main/java/com/hamdymo/adhan/prayertime/Cron/CronCreator.java
+++ src/main/java/com/hamdymo/adhan/prayertime/Cron/CronCreator.java
@@ -4,9 +4,11 @@ import com.hamdymo.adhan.prayertime.facade.FileFacade;
 import lombok.AllArgsConstructor;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 @AllArgsConstructor
 public class CronCreator {
@@ -24,27 +26,6 @@ public class CronCreator {
         Process proc = rt.exec(commands);
         System.out.printf("%s %s %s %s %s",commands[0],commands[1],commands[2], commands[3], commands[4]);
     }
-
-//    public void testingCronJobs() throws IOException, InterruptedException {
-//        Runtime runtime = Runtime.getRuntime();
-//        String[] commands = {"crontab", "-e"};
-//
-//        Process proc = runtime.exec(commands);
-//
-//        InputStream stdIn = proc.getInputStream();
-//        InputStreamReader isr = new InputStreamReader(stdIn);
-//        BufferedReader br = new BufferedReader(isr);
-//
-//        String line = null;
-//        System.out.println("<OUTPUT>");
-//
-//        while ((line = br.readLine()) != null)
-//            System.out.println(line);
-//
-//        System.out.println("</OUTPUT>");
-//        int exitVal = proc.waitFor();
-//        System.out.println("Process exitValue: " + exitVal);
-//    }
 
     public void addRerunCronjob() throws IOException {
         String test = String.format("""
@@ -64,5 +45,51 @@ public class CronCreator {
         BufferedReader stdInput = new BufferedReader(new
                 InputStreamReader(proc.getInputStream()));
         return stdInput.readLine();
+    }
+
+    public void createCrontabFromFile() throws IOException, InterruptedException {
+
+        // 1. Read the cron entries from the file.
+        String filePath = fileFacade.getFilenamePath(CRONTAB_TXT);
+        StringBuilder cronEntries = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // You might want to add validation here to check the format of each line.
+                // For example, using a regular expression to match cron syntax.
+                cronEntries.append(line).append("\n");
+            }
+        }
+
+        // 2. Construct the cron command.
+        String cronCommand = String.format("echo \"%s\" | crontab -", cronEntries.toString());
+
+        // 3. Execute the command using ProcessBuilder.
+        ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", cronCommand);
+        processBuilder.redirectErrorStream(true); // Redirect error stream for debugging
+
+        Process process = processBuilder.start();
+
+        // 4. Handle process output (recommended for debugging).
+        try (java.io.InputStream inputStream = process.getInputStream();
+             java.util.Scanner s = new java.util.Scanner(inputStream).useDelimiter("\\A")) {
+            String output = s.hasNext() ? s.next() : "";
+            System.out.println("Crontab update output:\n" + output);
+        }
+
+        // 5. Wait for the process to complete and check the exit code.
+        boolean finished = process.waitFor(10, TimeUnit.SECONDS); // Timeout
+
+        if (!finished) {
+            process.destroyForcibly();
+            throw new IOException("Crontab update timed out.");
+        }
+
+        int exitCode = process.exitValue();
+        if (exitCode != 0) {
+            throw new IOException("Crontab update failed with exit code: " + exitCode + ". Check output for errors.");
+        }
+
+        System.out.println("Crontab updated successfully from file: " + filePath);
     }
 }
