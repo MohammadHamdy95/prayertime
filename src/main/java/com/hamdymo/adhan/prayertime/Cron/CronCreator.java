61c4568222ad2b82a58191af1ee6b9d334d80c67
mohamdy <moham1255@gmail.com>
2025-02-08 02:34:52 -0800

    fixing url

--- src/main/java/com/hamdymo/adhan/prayertime/Cron/CronCreator.java
+++ src/main/java/com/hamdymo/adhan/prayertime/Cron/CronCreator.java
@@ -18,10 +18,9 @@ public class CronCreator {
     }
 
     public void createCronjobFileToCronjob() throws IOException {
-        Runtime rt = Runtime.getRuntime();
         System.out.println(fileFacade.getFilenamePath(CRONTAB_TXT));
         String[] commands = {"crontab", "<", fileFacade.getFilenamePath(CRONTAB_TXT)};
-        Process proc = rt.exec(commands);
+        Runtime.getRuntime().exec(commands);
     }
 
     public void addRerunCronjob() throws IOException {
