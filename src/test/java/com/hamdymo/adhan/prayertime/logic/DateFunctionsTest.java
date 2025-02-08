40eb172910d90a487319d39de39f02ba0c59520d
mohamdy <moham1255@gmail.com>
2025-02-08 15:19:33 -0800

    fixed typo

--- src/test/java/com/hamdymo/adhan/prayertime/logic/DateFunctionsTest.java
+++ src/test/java/com/hamdymo/adhan/prayertime/logic/DateFunctionsTest.java
@@ -1,10 +1,25 @@
 package com.hamdymo.adhan.prayertime.logic;
 
+import com.hamdymo.adhan.prayertime.facade.FileFacade;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class DateFunctionsTest {
+    @Mock
+    private FileFacade fileFacade;
+
+    @InjectMocks
+    private DateFunctions dateFunctions;
+
+
 
     @Test
     void getDateTomorrow() {
@@ -15,6 +30,8 @@ class DateFunctionsTest {
         String test = "14:30";
         int howMuchToAdd = 25;
 
+        String result = dateFunctions.addMinutesToHourMinuteString(test, howMuchToAdd);
+        assertEquals("14:55", result);
     }
 
     @Test
